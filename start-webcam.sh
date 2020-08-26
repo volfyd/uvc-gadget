e28a5f026beed82a34dfa69cb584bce53ef50213
Huan Truong <htruong@tnhh.net>
2020-08-26 00:07:03 -0700

    Tweak contrast and auto exposure settings
    The setting in start-webcam script seem too dark. Tweak the settings so
    that people don't look like hackers.

--- start-webcam.sh
+++ start-webcam.sh
@@ -3,8 +3,8 @@
 
 /opt/uvc-webcam/multi-gadget.sh
 /usr/bin/v4l2-ctl -c auto_exposure=0
-/usr/bin/v4l2-ctl -c auto_exposure_bias=8
-/usr/bin/v4l2-ctl -c contrast=20
+/usr/bin/v4l2-ctl -c auto_exposure_bias=15
+/usr/bin/v4l2-ctl -c contrast=0
 /usr/bin/v4l2-ctl -c video_bitrate=25000000
 
 /opt/uvc-webcam/uvc-gadget -f1 -s2 -r1  -u /dev/video1 -v /dev/video0
